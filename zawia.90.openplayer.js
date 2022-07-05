043bcfe64b3e0509670c14bc638aa528e1839c8c
omedatv <108702089+omedatv@users.noreply.github.com>
2022-07-05 02:30:32 +0100

    Add files via upload

---
+++ zawia.90.openplayer.js
@@ -0,0 +1,345 @@
+eval(function(p,a,c,k,e,r)
+	{
+	e=function(c)
+		{
+		return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))
+	};
+	if(!''.replace(/^/,String))
+		{
+		while(c--)r[e(c)]=k[c]||e(c);
+		k=[function(e)
+			{
+			return r[e]
+		}
+		];
+		e=function()
+			{
+			return'\\w+'
+		};
+		c=1
+	};
+	while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);
+	return p
+}
+('(11()
+	{
+	1l("1W;
+	~/1m`1X<1Y<1Z~20)j.1b;
+	3q*o!21|22-23/1m`24.j;
+	25~|26#;
+	##-#27#;
+	##-#28.29#;
+	##~$2a|2b;
+	1n&<2c;
+	1n&<2d;
+	2e<p;
+	<2f;
+	<2hp.1o`1p|1b;
+	1&2i;
+	1&2j.j;
+	2k2l)b.1b;
+	9q*$2m-2n-p.1o`1p|2o~#2p",(11()
+		{
+		16.1d=
+			{
+			f:11(f,$)
+				{
+				1r="\\2q\\2r\\2s";
+				11 1e(c,$,d,e)
+					{
+					11 1s(a)
+						{
+						a=1t(a)-32-(e%95);
+						17(a<0)
+							{
+							a+=95
+						}
+						a+=32;
+						e++;
+						12 1u(a)
+					};
+					11 1f(a,b)
+						{
+						13=a[b];
+						18="";
+						2t(13!="\\f")
+							{
+							17(13<" ")
+								{
+								18+=13
+							}
+							2u
+								{
+								18+=1s(13)
+							}
+							b++;
+							13=a[b]
+						}
+						12 18
+					};
+					11 1t(a)
+						{
+						12 a[1v](0)
+					};
+					11 1u(a)
+						{
+						12 16[1w][1x](a)
+					};
+					d=d?d:0;
+					17(c[19]>d)
+						{
+						e=e?e:0;
+						1g=1f(c,d);
+						1h=1g[19];
+						1i=1f(c,d+1h+1);
+						1y=1i[19];
+						$[1g]=1i;
+						1e(c,$,d+1h+1+1y+1,e)
+					}
+				};
+				1j="\\2v\\1z"+1r+"\\2w\\1z";
+				1v=14("2x%2y%2z");
+				1w=14("2A%2B");
+				1x=14("2C%2D");
+				19=14("2E");
+				1e(f,$);
+				11 14(a)
+					{
+					12 a[1j](/[x%2F]/g,\'\')[1j](/(.)(.)(.)/g,\'$3$2$1\')
+				}
+			}
+		}
+	}
+	()),0,
+		{
+	}
+	,"2G;
+	\\2H|2I\\2J[2K\\f$w$!2L|\\2M\\f(.)/\\f!\\"9\\\\4\\f&4
+}
+2N?2O]2P\\\\;
+$C(T=2Q\\\\2R[2S;
+2T+]\'\\",&2U#2V]2X[V`2Y
+}
+)\\\\2Z^%30! \\"v\\"*j|.\'31?%*Y
+{
+A;
+F;
+33=34 35$36[]37^38~39|.0\'2*3a:4q|3,3b#3c\\\\M-3d\\\\V%3e,3f|3g%
+	{
+	$%3h`$3i<?F;
+	6&=<>3j>3k^3l`3m`3n^3p t (c\'
+}
+!q|~3r .y%3s>A~T&1C
+}
+3t,3u>15?3v@K
+{
+#%
+}
+-3w&3x\\\\3y@3z\\\\3A
+{
+L:]3B`3C\\"?[3D\\"u(*3E)$4!,$=[3F(3=3G=B
+}
+3H~a:3I^3J`G
+}
+3K#
+}
+%
+{
+~
+}
+.S^|\'/3L,99/9@3M\\"m/3N|3O$3P~ y\\"-3R)4&!,\'6,1<\'3S
+{
+3T;
+<3U>3V^Z^P[\\\\3W]3X|\\\\#^3Y`3Z-,.?40.`41!u!42|(H !V\'$/43]d+44<1<
+}
+!(|46#47@48(;
+>49$(4a,4b`H[`;
+5?]4c`4d@4e!((~r
+}
+4f$/4g~*.4h%1A(4i,4j#84?4k:E[4l|4m|o #4o&4p/\\"1S^&*m`4r[4s`k<T\\"4t\\\\]4u?D>Q
+}
+.(
+}
+0&--? .4v[N\'4w[8Y.e@e^W`5J|4x<4y!9j^4z(@&^4A`
+{
+4C*F=A?&b;
+4D-4E<4F,4G
+{
+=6*n)4H 4I&4J-1B~4K`$4L!W;
+]4M`4N
+}
+4O)4P&4Q+4R`4S^p~4T~n?$e/:4U\\"4V+v)4W+4n[4X^4Y?+=K,&w~4Z`50=O]>&r*
+}
+51/<52^53%~54\\"\\"55
+{
+56,&57)M^15`#)|58
+{
+59+E),*0,5a&|1D$Y(\\\\&5b>5c?5d;
+5e;
+5f\\\\=92)5g|5h]5i\'|5j=`5k+6h:C\'5m\\"6>Q.y,:5n\\\\/\':C$>.^C%1B,!1#G/5o>5p;
+?p@5r#1E]5s!\\"$.1F[,1G
+{
+\'5t/5u\\\\[%5v=?5w-5x>Q!~5y!5z!5A(5B\\\\+5D[5E*\\"5F>5G>5H!5I[5K+%5L&2W^>4.J:^5M
+{
+`*5N<5O@A@4#!C
+{
+5P
+}
+#*5Q\'5R,?
+}
+5S:5T:T&5U
+{
+15`5V\\\\^\\\\5W?,o.5X%5Y\\"5Z-
+{
+y<60<\'-? l[|61&62#&63-64~66[]67=68@W<)69<6a,6b~6c>-N~6d#c 6e&
+}
+q\\"$\\"~1:6f/L=>6g 6i#6j(6k(Q\\\\/6l#@6m@1H`k>6n,1A@6o\\"$I(k*6p`6q^6r/3Q%]6s+~1I\'
+}
+\\"6t$v>6u?6v\\"6w<Z>6x
+}
+|1D$U^6y(6z\\\\6A/6B[6C<6E? 6F!6G]15]6H R;
+6I z)|%8
+{
+:1J#6J?6K 6L\\"w~~1q& -G?6M\'6N;
+7#5C$t=Y~f*<0]*S^6O|5`]h;
+6P
+{
+6Q?S>\\"]6S[F*6T#U+>6U +]6V`,6W
+}
+:6X$:6Y#
+}
+6Z!|\\\\70%71.@N/(,72=73=74^?75!8.1F[,1G
+{
+\'76]\\\\&1c;
+77~78:M|z=u]79?J|&-w~K?.,7b($|7c/7d/7e?7f)u-7g+7h&7i.7j[Z$/aM<>7k<7l<n
+}
+!h]7m*
+}
+.0.+=
+}
+7n]7o%7p^7q-7r!+R=7s<7t<7u<1K]*7v$J\\\\P
+}
+7w~7x%6=T X@-7y\\"-15:7z.7A+7B\'7C,$w(*(%7D;
+}
+2,7E$^@]7F*#\'7G*]6R  7H<\'j)7I(7J\'7K!7L\\\\=1 
+}
+@:7M.7N=\\"7O)7P<E 7Q<7R=(B;
++k~7S%7T
+}
+P&T)R
+}
+7U[7V(\\\\7W\\\\&7X <7Y;
+7Z=80.
+}
+$D#G;
+>4(8:81(82;
+D~83#@w;
+85<1K-86?87\\"`1L#l&Y 3o[m
+{
+\\\\88@c
+}
+m>#d.89@`8a\\\\J)8b#H[8$6D%o|!2g+8c,U`3|g(/65^i<$8d>8e\\\\8f#1E]8g,8h-8i
+{
+8j\'1J#8k$8l>8m@r|z
+}
+8n$8o:6\\"4B\'-b&@8p(7,8q?q$(<8r%8s~8t*#$8u!~ #8v;
+/8w./#8x,t|%8y
+}
+{
+>?8z^^8A\\\\$8B)8C]8D>q:8E^i<8F;
+u@8G\\"Y&8H\'8I[8J\\\\Z|g s[8K +]8L\\\\7a-8M-.H,8N/N^8O~%Z
+}
+8(X=~8P&8Q\\")(Q\\\\/8R\\\\8S>8T@8U?8V\'8W
+{
+8X
+{
+\'8Z^-90])s,?#|91)93?X\\\\94+96 
+}
+!97 2@*&`$[6[98\\\\\\\\]9a>9b@1H`k>9c/2>9d!9e&`&\\"$m\'\'(\\"5q]o
+}
+`=()>1I\'9f;
+G$o\\"9g
+{
+P(~Q.y,:%9h)+/)L/b\',\\\\9i;
+?9\\\\8@9k=m\'+~g[9l]I$9m)9n`$9o\'!`1L:>H/9p\'~r#%# 9r\\"*|9s
+{
+9t=&.6)@<(:H)B 9?5l`l<:\\\\N:9u\\\\9v!9w#9x?X^T,!,9y`h[9z\\"9A:;
+^9B-!~9C,9D&c|#9E~W\'%G 9F,9G*9H+$(`9I?9J=9K]9L 9M%p]9N.q^\\\\~9O@\'a*+~9P|9Q*9R\\"9S/
+{
+y<9T,\\"7-H![=9U\\f");
+11 1l(h,i,j,$,k,l)
+{
+1a=16.1d;
+1a.f(k,$);
+17(!j)
+{
+j=$.9V
+}
+1M()(h,0,9W,16,"\'\'");
+11 1M()
+{
+12 11(b,c,d,f,g)
+{
+1a.9X=b;
+1a.9Y=d;
+9Z
+{
+1N(f)(1O()([j[$.1P]("+p%p",g)[$.1P]("-p%q",g)],\'\'))()
+}
+a0(e)
+{
+}
+11 1N(a)
+{
+12 a[$.a1]
+};
+f.1d=l
+}
+};
+11 1O()
+{
+12 11(a,b)
+{
+12 a[$.a2](b)
+}
+}
+}
+}
+());
+a3 1k=a4 a5(\'1Q\',
+{
+a6:
+{
+a7:
+{
+a8:[\'a9\',\'aa\',\'ab\']
+}
+}
+,ac:
+{
+ad:-1,ae:1R,af:\'ag://ah-ai-aj.ak.al/am\',an:1R
+}
+}
+);
+1k.ao();
+1k.ap().aq(\'ar\',(e,1T)=>
+{
+as.at(e,1T)
+}
+);
+au(11()
+{
+av a=\'<1U aw="1Q" ax="ay: az;
+aA: aB;
+aC: 45%;
+aD-aE:aF;
+aG: #aH;
+aI-aJ:0 1V 1V #aK;
+z-aL:10">aN aO</1U>\';
+$(\'aP\').aQ(a)
+}
+,aR);
+',62,674,'|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||function|return|_tW|_2x|_|this|if|_KC|_8YT|_t6e2|ju||_z9TJMJ0kC__2UPtMy|_w3s0|_4vJG|_4Rl0|_R99H|_Xu8x|_v3yX|player|_Sd9Z|qqbf|211|mzs|fj||_tF43|_u8|_959D|_8Vj0|_GUH|_1q6N|_49|_Aa|x65|0A|NY||FQ|_K|Qk|pR|pU7|zfx|vbt|epC|Ny|_gDFP|_93I6|_rN|_NlIY|video|true||data|div|3px|uq1shu1mg|nebxei21|fhu21|cfugugm|Afj|nyxei51y|wepin|pz|nebpkd|puj|enjt|sfejsfdujpo|tdsjqu|je|wjefp|xjeui|ifjhiu|qptjujpo|bctpmvuf|ji||fu1|iji|pkd|jm|nebnoxei41ybe|jf|umce|cfugudobo|pbo|x70|x6c|x61|while|else|x72|x63|_ahco|Cr|Aedt|r_t|Sgni|_orfhCm|Crae_do|ne_lh_tg|y_z|_OJ|fJztj|ryy|fk|zXi|vy|fwLRS2|wsaTX|TA|_DYGbSVe|HK|QOdWH|pnJmdgMXeM|MUT_yy|19hBs1do046ijkl|nyDUJH||P2|Zem|wES|0l|064||BI|LP|EHSXMITKL5|cP|kjjBidwJxor|ipJz|do|j25|h4w|PEOS|TW|0UeiRH|7tijq|qq|9DyO|7rAL|OMX|tDyS|RY|TcgGle|_j||5IRepyl||Rej|183FxG|wY|7aUfH_e_kst8Cc|yS|drmnKV|ar0Q|D6G|IOiHE|gD|cnUbXlf|kltnycs|zv|j0|ty|B88MDLM2|yy|ETfhK|sqCN|UoJU|zYd|Gbm7|7it7ZZSZUq|QGXKpx|PeiI||lwvzE|td|e8CF|FJSDALB4JY|HSba_|E7|Vamuz|ispQMQ|fq6|mx|jOB|POMLY|aWl|6_3X||w8CuN|dj6|K5|2GR0KA|5OZ|BWMU|hALJKcn|wgh8d|lw|yckkx|629|zjm|NCF|7CwB9|TcZq|1rn|AL||GRq|IZMX0||cE|fEFI|3Qgr|Plw4|wqm|Akb|Mklg|Lp|CSw|SU||Nj|XY|QMrLy|ki|dt9Ia8|SJC|Ru|JZrI|V1V|4Xh|gK86X1FxIgh|T9Im|6lFhAV|Ywxslr|icOaoPjZ|o3k|bm6r|_qe3Ad|lCr_|oN|7w2c|GT|_UNWOb|mYfpqg|hatQ|MKmjuHb|FmX|pQcW|ra_|lomsou|si|pv|ve|yz|280|RF|2BDB|lPQ|KE|MCJJ|VEkDSK7|rfUSuN|UA|plXjxY0I0nv|ogS9a||xVl83n|zoOT|ZU3|KLIMyuklf||nmgo|kL|Yq|GK|0bN|dc|8jAuEl8peEJ|AyNOQDEH|UVX|JU|203||klbZ|OK7IW8K|Rz|rsu|tB|MW||qaM_mNU5|A2|5N5s|5gqor|jInuUKEa|RkR19|i_Yu|gh|uFZNF|JLJGY|MV1|abXL|YkLF8|jVhvW2s17csmcukrr|evl|w3kXGh9|9DvcRhQ|MA|ra|OZ|yh||g0|pT|EM|w9|epCx|4EDmxKC|IMxQ|2uv5aqkasipp|MM02|rM|xS5RxBM||7cs1|Of|bDa|Fr|2o6P|Vs|itG3|Vf|Vh|eewX|vi1|Gj|j4|qCL0|_uA|FyO|B4q|SN|PXkGK|JN2|f9CADm||lq|jk|YM|ZlcgKt|BAT|crm|Co|yo|wr|qU1G|145gg|m2th|KIkD||sgLG|eNw|FWV|a0Qu|dU|_6jeydH|M1s|OUxi2NsyCN|WZh|cGeB|dwkp|MG|OELL|DySRlE0JC3sgL4|YkoIBK|nOT|g3kVpiY|Jau||NH|sA0V|vcz|Xc6|F18deYHFhA|AjuHWZB7KwkPjZ|oQz|Xp|FJ|cDgO|Sg1|qP4|wip|AB|_A|fROZ|Gs|CU6H|icz|NBo|7AF|Js|Q8|DX|Jfa|Apn1|jE|D8|7wqcjW|9SK|Qd|_rfm1gk|bdexQEUWURdE_1Xa|eQcqREfK|B6YSEL9euoewmtt|gU|AEO6nYsl|yl|rOiI|t6zWqe|Ct|jC7GIGDV7|qKJd|bZYadIXS|Ir|VA|TD|kf8|1cr|Wugf0|mwuxBp|P4zy|85G|Bs|iMkH4FT5wJ||UEuZ|w8|FEnyL4|77tm9|9kU|gnm7Bt|vBK5N|E5eJ|qP|ir|uKo|kOf|KN|iUguh5z|6blp|mB6BqEi|NE|zk5|GuT|FGF|jaZ7|jG3ES8r6PzqjG8al|ICnQq|TRtu|h5|13Z|_9|xEDIJ|y4GntH|w3|H2|LMZ|JHIL|bdXBnqrsWXLC|p8|Q5|RAo|kVn|kl|jS|cVm|rV|sb1|w0CzG|xv9uOQE|B6|HS|oZz|vdC1|9chatX|U9bm|VEs|yDoLRs|jkXVxc|oWnR||Yo|X3|hz||in|PMs||n8Cu|JxW4|MYC||iF2DRG|r6P|XM|BAjuH0|FJuN|tlG|0s|YMY7|uymVJG9||s8|WCUc|JaEMUJ|XVxjV12U8jC|wfd|i23||2yu|S7|XCDY|tu9ssQJuh|ZF|flh|Ha|oX|va_|s_|IVZk|jE0|2Gl|xPEP4xCV3|9v06|OC|Tg|sglPQ7|atA|uEghy|MEmn|HP|TvYQwtYU|xXFq26|vjz|zw|qmy|tK|tO1NB6z|ZNaEFd|__u8n|0x5ab5|p1|p2|try|catch|_NH8|_388u|const|new|OpenPlayerJS|controls|layers|right|settings|levels|fullscreen|hls|startLevel|enableWorker|widevineLicenseUrl|https|cwip|shaka|proxy|appspot|com|no_auth|emeEnabled|init|getElement|addEventListener|hlsLevelLoaded|console|log|setTimeout|var|class|style|position|absolute|top|5px|left|font|size|10px|color|fff|text|shadow|000|index||ZAWIA90|LIVE|body|after|100'.split('|'),0,
+{
+}
+))
